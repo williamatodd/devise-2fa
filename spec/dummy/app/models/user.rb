2ddce252e3e545580bd6932ec33fd376443ccc56
Alex Piechowski <alex@piechowski.io>
2019-06-24 12:00:52 -0500

    Add mongoid and multiple rubies to test suite

--- spec/dummy/app/models/user.rb
+++ spec/dummy/app/models/user.rb
@@ -1,6 +1,38 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :two_factorable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+# frozen_string_literal: true
+
+require 'symmetric_encryption'
+
+if defined?(ActiveRecord)
+  class User < ApplicationRecord
+    devise :two_factorable, :database_authenticatable, :registerable,
+      :recoverable, :rememberable, :validatable
+  end
+else
+  require 'mongoid'
+  class User
+    include Mongoid::Document
+    field :encrypted_otp_auth_secret,       type: String, encrypted: true
+    field :encrypted_otp_recovery_secret,   type: String, encrypted: true
+    field :otp_enabled,                     type: Boolean, default: false
+    field :otp_mandatory,                   type: Boolean, default: false
+    field :otp_enabled_on,                  type: DateTime
+    field :otp_failed_attempts,             type: Integer, default: 0
+    field :otp_recovery_counter,            type: Integer, default: 0
+    field :otp_persistence_seed,            type: String
+    field :otp_session_challenge,           type: String
+    field :otp_challenge_expires,           type: DateTime
+
+    index({ otp_session_challenge: 1 }, background: true)
+    index({ otp_challenge_expires: 1 }, background: true)
+
+    devise :two_factorable, :database_authenticatable, :registerable,
+      :recoverable, :rememberable, :validatable
+    field :email,              type: String, default: ''
+    field :encrypted_password, type: String, default: ''
+    field :reset_password_token,   type: String
+    field :reset_password_sent_at, type: Time
+    field :remember_created_at, type: Time
+    field :name, type: String
+    field :phone, type: String
+  end
 end
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
+require_relative Rails.root.join('app/models/user')
+
+
 Rails.application.routes.draw do
   devise_for :users
-  root to: "application#show"
+  root to: 'application#show'
 end
